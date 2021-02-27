858f59e095d99d2314744853028102e709a336cf
volatileColors <volatilecolors@gmail.com>
2021-02-27 16:34:36 +0545

    Fix

--- editorOperations.h
+++ editorOperations.h
@@ -3,7 +3,6 @@
 
 void editorInsertChar(int c);
 void editorDelChar(void);
-
 void editorInsertRow(int at, char *s, size_t len);
 void editorUpdateRow(struct erow *row);
 void editorFreeRow(struct erow *row);
@@ -12,7 +11,6 @@ void editorRowInsertChar(struct erow *row, int at, int c);
 void editorRowDelChar(struct erow *row, int at);
 void editorRowAppendString(struct erow *row, char *s, size_t len);
 void editorInsertNewline(void);
-void editorIndent(struct erow *row);
 int editorRowRxToCx(struct erow *row, int rx);
 int editorRowCxToRx(struct erow *row, int cx);
 
